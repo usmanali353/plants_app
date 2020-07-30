e073afbbfb6290a32ea86cf40756329ee161d785
IIB <aliusman353@gmail.com>
2020-07-30 12:01:22 +0500

    Fixed forming Procedures

--- app/src/main/java/fyp/plantsapp/wheat_farming_procedures.java
+++ app/src/main/java/fyp/plantsapp/wheat_farming_procedures.java
@@ -7,19 +7,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 import fyp.plantsapp.Adapters.procedures_list_adapter;
 
 public class wheat_farming_procedures extends AppCompatActivity {
-  RecyclerView proceduresList;
+  PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wheat_farming_procedures);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-       proceduresList=findViewById(R.id.procedures_list);
-       proceduresList.setLayoutManager(new LinearLayoutManager(this));
-       proceduresList.setAdapter(new procedures_list_adapter());
+        pdfView=findViewById(R.id.pdfView);
+        pdfView.fromAsset("farming_procedures.pdf").load();
     }
 
 }
